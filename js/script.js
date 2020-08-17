113b25439a814bfdb049ce0ccf0a23aa103e65dd
Luis Felipe Jimenez Roldan <luisfelipejimenezroldan@gmail.com>
2020-08-17 13:18:11 -0400

    assets con js

---
+++ js/script.js
@@ -0,0 +1,20 @@
+$(function() {
+
+    $("a").click(function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var gato = this.hash;
+
+            $("html, body").animate({
+                scrollTop: $(gato).offset().top
+            }, 1000, function() {
+                window.location.hash = gato;
+            });
+        }
+    });
+    $('[data-toggle="popover"]').popover();
+});
+
+$(document).ready(function() {
+    $('[data-toggle="tooltip"]').tooltip();
+});
